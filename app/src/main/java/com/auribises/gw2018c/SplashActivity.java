054d78ba43cd372151e19ab9ef65391f9d86174d
Ishant Kumar <er.ishant@gmail.com>
2018-10-24 13:08:40 +0530

    Session21 - LocationServices, LocationRequest API and Animations in Android

--- app/src/main/java/com/auribises/gw2018c/SplashActivity.java
+++ app/src/main/java/com/auribises/gw2018c/SplashActivity.java
@@ -2,12 +2,20 @@ package com.auribises.gw2018c;
 
 import android.app.Notification;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    TextView txtTitle;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +23,22 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
 
-        handler.sendEmptyMessageDelayed(101,3000);
+        txtTitle = findViewById(R.id.textView8);
+        imageView = findViewById(R.id.imageView);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        //Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        txtTitle.startAnimation(animation);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable)imageView.getBackground();
+        animationDrawable.start();
+
+
+        //handler.sendEmptyMessageDelayed(101,3000);
     }
+
+    // Assignment: Create a SlideShow with Animations and MediaPlayer
+    // alpha, rotate, scale and translate, set : Tween Animations
 
     Handler handler = new Handler(){
 
