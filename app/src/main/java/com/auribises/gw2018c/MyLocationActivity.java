cd601b8e356bd389c6dce0bc8fca00b1c468ea34
Ishant Kumar <er.ishant@gmail.com>
2018-10-24 12:41:03 +0530

    Session21 - LocationServices and LocationRequest API

--- app/src/main/java/com/auribises/gw2018c/MyLocationActivity.java
+++ app/src/main/java/com/auribises/gw2018c/MyLocationActivity.java
@@ -8,6 +8,8 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +38,61 @@ public class MyLocationActivity extends AppCompatActivity
     StringBuffer buffer;
 
     ProgressDialog progressDialog;
+
+    // Access Google Web Services : LocationServices
+    GoogleApiClient apiClient;
+    GoogleApiClient.Builder builder;
+
+    GoogleApiClient.ConnectionCallbacks connectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+
+            /*if (ActivityCompat.checkSelfPermission(MyLocationActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MyLocationActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MyLocationActivity.this, "Please Grant Location Permissions", Toast.LENGTH_LONG).show();
+            } else {
+                Location location = LocationServices.FusedLocationApi.getLastLocation(apiClient);
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                txtLocation.setText(latitude + " : " + longitude);
+            }*/
+
+            fetchLocationUpdates();
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+
+        }
+    };
+
+    GoogleApiClient.OnConnectionFailedListener connectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        }
+    };
+
+    com.google.android.gms.location.LocationListener locationListener = new com.google.android.gms.location.LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            txtLocation.setText(latitude + " : " + longitude);
+
+            // Reverse Geocoding
+        }
+    };
+
+    void initGoogleApiClient() {
+
+        builder = new GoogleApiClient.Builder(this);
+        builder.addConnectionCallbacks(connectionCallbacks);
+        builder.addOnConnectionFailedListener(connectionFailedListener);
+        builder.addApi(LocationServices.API);
+
+        apiClient = builder.build();
+
+    }
 
     void initViews() {
         txtLocation = findViewById(R.id.textViewLocation);
@@ -50,17 +112,37 @@ public class MyLocationActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_location);
         initViews();
+        initGoogleApiClient();
+    }
+
+    void fetchLocationUpdates() {
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000); // 10 secs
+        locationRequest.setFastestInterval(5000); // 5secs
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this,"Please Grant Location Permissions",Toast.LENGTH_LONG).show();
+        }else {
+            LocationServices.FusedLocationApi.requestLocationUpdates(apiClient, locationRequest, locationListener);
+        }
+
+
     }
 
     @Override
     public void onClick(View v) {
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this,"Please Grant Location Permissions",Toast.LENGTH_LONG).show();
         }else {
             progressDialog.show();
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10, 10, this);
-        }
+        }*/
+
+        // Request to create a connection with Google Location Services
+        apiClient.connect();
 
     }
 
